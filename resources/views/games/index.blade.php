40c3bd29c51df5708c6cbc3f2086714282189d44
Rabaz Abdulkarim <rabazkooa@gmIl.com>
2026-09-24 12:42:55 +0200

    Genre toegevoegd aan game overzicht

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -16,6 +16,7 @@
                     <th>ID</th>
                     <th>Game</th>
                     <th>Platform</th>
+                    <th>Genre</th>
                     <th>Rating</th>
                 </tr>
             </thead>
@@ -26,6 +27,7 @@
                         <td>{{ $game->id }}</td>
                         <td>{{ $game->game_name }}</td>
                         <td>{{ $game->platform }}</td>
+                        <td>{{ $game->genre }}</td>
                         <td>{{ $game->rating }}/10</td>
                     </tr>
                 @endforeach
